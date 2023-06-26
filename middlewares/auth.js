4f51669b3101201fa113a540f7c09a5ea6668bc5
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-06-26 10:36:10 +0400

    Исправил auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,8 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
 
   let payload;
@@ -17,7 +18,8 @@ module.exports = (req, res, next) => {
       NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
     );
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
 
   req.user = payload;
